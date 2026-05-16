01c0b6c74b8c55a4f6fd99c492b902ff3875de5d
auraaiemail <auraaiemail@gmail.com>
2026-05-16 05:55:44 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+// Service worker disabled - clears all old caches
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', ev => {
+  ev.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+      .then(() => self.clients.matchAll({type:'window'}).then(clients => {
+        clients.forEach(c => c.navigate(c.url));
+      }))
+  );
+});
